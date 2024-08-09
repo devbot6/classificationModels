e6fcaf25d4f635feafe2ab9041992769a379c4bc
Devon <devhulse04@icloud.com>
2024-08-09 10:21:04 -0400

    added hyperparameters to decisiontree classifier

--- Notes/DecisionTreeTest.py
+++ Notes/DecisionTreeTest.py
@@ -35,13 +35,17 @@ plt.plot(data[n_data_1:n_data_1+n_data_2, 0], data[n_data_1:n_data_1+n_data_2, 1
 plt.plot(data[n_data_1+n_data_2:, 0], data[n_data_1+n_data_2:, 1], 'r.', ms=12, label="Label=2")
 plt.legend(fontsize=15)
 
-classifier = DecisionTreeClassifier()
+classifier = DecisionTreeClassifier(criterion='log_loss', splitter='best', max_depth=None, min_samples_split=2,
+                                    min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None,
+                                    min_impurity_decrease=0.0, class_weight=None, ccp_alpha=0.0)
 
 classifier.fit(data, labels)
 
 train_predictions = classifier.predict(data)
 
 print(classification_report(train_predictions, labels))
+
+print()
 
 new_point = np.array([[6, 0 ]])
 
